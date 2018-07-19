405d13b4ea8d0ad5cfb9bcb2de45282317365a9e
mandeep <mandeep@users.noreply.github.com>
2018-07-18 20:13:12 -0700

    Use vector to hold coordinates

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate nalgebra;
 
 use std::fs::File;
 
+use nalgebra::core::Vector3;
+
 fn main() {
     let (width, height): (u32, u32) = (1600, 1600);
 
@@ -10,9 +12,12 @@ fn main() {
 
     for x in 0..width {
         for y in 0..height {
-            let red = (255.99 * (x as f64 / width as f64)) as u8;
-            let green = (255.99 * (y as f64 / height as f64)) as u8;
-            let blue = (255.99 * 0.2) as u8;
+            let coordinate: Vector3<f64> = Vector3::new(x as f64 / width as f64,
+                                                        y as f64 / width as f64,
+                                                        0.2);
+            let red = (255.99 * coordinate.x) as u8;
+            let green = (255.99 * coordinate.y) as u8;
+            let blue = (255.99 * coordinate.z) as u8;
             buffer.put_pixel(x, y, image::Rgb([red, green, blue]));
         }
     }
