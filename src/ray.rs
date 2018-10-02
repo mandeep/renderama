7e0a822f73ec0755d8ee2688cdf052c0071f3ebc
mandeep <mandeep@users.noreply.github.com>
2018-10-02 19:38:12 -0400

    Add method to test if a ray hit point on sphere

--- src/ray.rs
+++ src/ray.rs
@@ -16,6 +16,10 @@ impl Ray {
     }
 
     pub fn color(&self) -> Vector3<f64> {
+        if self.hit_sphere(&Vector3::new(0.0, 0.0, -1.0), 0.5) {
+            return Vector3::new(1.0, 0.0, 0.0);
+        }
+
         let unit_direction: Vector3<f64> = self.direction.normalize();
         let point: f64 = 0.5 * (unit_direction.y + 1.0);
 
@@ -24,5 +28,15 @@ impl Ray {
 
     pub fn point_at_perimeter(&self, point: f64) -> Vector3<f64> {
         self.origin + point * self.direction
+    }
+
+    pub fn hit_sphere(&self, center: &Vector3<f64>, radius: f64) -> bool {
+        let origin_center: Vector3<f64> = self.origin - center;
+        let a: f64 = self.direction.dot(&self.direction);
+        let b: f64 = 2.0 * origin_center.dot(&self.direction);
+        let c: f64 = origin_center.dot(&origin_center) - (radius * radius);
+        let discriminant: f64 = b * b - 4.0 * a * c;
+
+        return discriminant > 0.0;
     }
 }
