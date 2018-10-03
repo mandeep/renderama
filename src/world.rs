aa665c5bc52534f2afbeb9a585e1749ac25dcbe5
mandeep <mandeep@users.noreply.github.com>
2018-10-03 15:55:53 -0400

    Refactor code to use Rust idioms rather than OOP idioms
    Move the hitable list into a separate world.rs file so that
    the hitable.rs file isn't as confusing. Thanks to projects like
    rayn and RustyRaytracer for the inspiration.

---
+++ src/world.rs
@@ -0,0 +1,41 @@
+use hitable::{Hitable, HitRecord};
+use nalgebra::core::Vector3;
+use ray::Ray;
+
+
+pub struct World {
+    pub objects: Vec<Box<dyn Hitable>>
+}
+
+
+impl World {
+    pub fn new() -> World {
+        World { objects: Vec::new() }
+    }
+}
+
+
+impl Hitable for World {
+    fn hit(&self, ray: &Ray, position_min: f64, position_max: f64) -> Option<HitRecord> {
+        let mut record = HitRecord::new(0.0, Vector3::zeros(), Vector3::zeros());
+        let mut hit_anything: bool = false;
+        let mut closed_so_far: f64 = position_max;
+
+        for object in &self.objects {
+            match object.hit(ray, position_min, closed_so_far) {
+                None => (),
+                Some(hit_record) => {
+                    hit_anything = true;
+                    closed_so_far = hit_record.parameter;
+                    record = hit_record;
+                }
+            }
+        }
+
+        if hit_anything {
+            Some(record)
+        } else {
+            None
+        }
+    }
+}
