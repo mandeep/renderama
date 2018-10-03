90addeba1753c6e7f7e42e7cc5f7ff4abf4f2cd5
mandeep <mandeep@users.noreply.github.com>
2018-10-02 22:40:47 -0400

    Implement hitable for spheres

---
+++ src/sphere.rs
@@ -0,0 +1,46 @@
+use nalgebra::core::Vector3;
+
+use hitable::{Hitable, HitRecord};
+use ray::Ray;
+
+
+pub struct Sphere {
+    pub center: Vector3<f64>,
+    pub radius: f64
+}
+
+
+impl Sphere {
+    pub fn new(center: Vector3<f64>, radius: f64) -> Sphere {
+        Sphere { center: center, radius: radius }
+    }
+}
+
+
+impl Hitable for Sphere {
+    fn hit(&self, ray: &Ray, position_min: f64, position_max: f64, hit_record: &mut HitRecord)
+            -> bool {
+        let sphere_center: Vector3<f64> = ray.origin - self.center;
+        let a: f64 = ray.direction.dot(&ray.direction);
+        let b: f64 = sphere_center.dot(&ray.direction);
+        let c: f64 = sphere_center.dot(&sphere_center) - (self.radius * self.radius);
+        let discriminant: f64 = b * b * a * c;
+
+        if discriminant > 0.0 {
+            let first_root: f64 = (-b - (b * b - a * c).sqrt()) / a;
+            let second_root: f64 = (-b + (b * b - a * c).sqrt()) / a;
+            let roots = vec![first_root, second_root];
+
+            for root in roots {
+                if root < position_max && root > position_min {
+                    hit_record.position = root;
+                    hit_record.point = ray.point_at_perimeter(hit_record.position);
+                    hit_record.normal = (hit_record.point - self.center) / self.radius;
+                    return true;
+                }
+            }
+
+        }
+        false
+    }
+}
