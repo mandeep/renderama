ae549fece8e149887f5241e4bfaec7954f9063eb
mandeep <mandeep@users.noreply.github.com>
2018-10-03 00:10:58 -0400

    Add samples to enable anti-aliasing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 extern crate image;
 extern crate nalgebra;
+extern crate rand;
 
+mod camera;
 mod hitable;
 mod ray;
 mod sphere;
 
-use std::fs::File;
-use nalgebra::core::Vector3;
+use camera::Camera;
 use hitable::HitableList;
+use nalgebra::core::Vector3;
 use sphere::Sphere;
+use std::fs::File;
 
 
 fn main() {
     let (width, height): (u32, u32) = (1600, 800);
+    let samples: u32 = 100;
 
     let mut buffer = image::ImageBuffer::new(width, height);
-
-    let lower_left_corner = Vector3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vector3::new(4.0, 0.0, 0.0);
-    let vertical = Vector3::new(0.0, 2.0, 0.0);
-    let origin = Vector3::new(0.0, 0.0, 0.0);
+    let camera = Camera::new(Vector3::new(-2.0, -1.0, -1.0),
+                             Vector3::new(4.0, 0.0, 0.0),
+                             Vector3::new(0.0, 2.0, 0.0),
+                             Vector3::new(0.0, 0.0, 0.0));
 
     let mut world = HitableList::new();
     world.push(Box::new(Sphere::new(Vector3::new(0.0, 0.0, -1.0), 0.5)));
@@ -27,12 +30,16 @@ fn main() {
 
     for x in 0..width {
         for y in 0..height {
-            let u = x as f64 / width as f64;
-            let v = y as f64 / height as f64;
+            let mut coordinate: Vector3<f64> = Vector3::zeros();
 
-            let ray = ray::Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-            let coordinate = ray.color(&world);
+            for sample in 0..samples {
+                let u = (x as f64 + rand::random::<f64>()) / width as f64;
+                let v = (y as f64 + rand::random::<f64>()) / height as f64;
+                let ray = camera.get_ray(u, v);
+                coordinate += ray.color(&world);
+            }
 
+            coordinate /= samples as f64;
             let red = (255.0 * coordinate.x) as u8;
             let green = (255.0 * coordinate.y) as u8;
             let blue = (255.0 * coordinate.z) as u8;
