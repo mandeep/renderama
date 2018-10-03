deb23fdd484fa6219bb5e786c93521f9279fee7d
mandeep <mandeep@users.noreply.github.com>
2018-10-02 22:41:13 -0400

    Use hitables in ray color calculation

--- src/ray.rs
+++ src/ray.rs
@@ -1,6 +1,6 @@
-extern crate nalgebra;
-
+use std::f64;
 use nalgebra::core::Vector3;
+use hitable::{Hitable, HitRecord};
 
 
 pub struct Ray {
@@ -15,13 +15,13 @@ impl Ray {
         Ray { origin: a, direction: b }
     }
 
-    pub fn color(&self) -> Vector3<f64> {
-        let point: f64 = self.hit_sphere(&Vector3::new(0.0, 0.0, -1.0), 0.5);
-        if point > 0.0 {
-            let normal = (self.point_at_perimeter(point) - Vector3::new(0.0, 0.0, -1.0)).normalize();
-            return 0.5 * normal.map(|coordinate| coordinate + 1.0);
+    pub fn color(self, world: &Hitable) -> Vector3<f64> {
+        let mut hit_record = HitRecord::new(f64::MAX);
+        if world.hit(&self, 0.0, f64::MAX, &mut hit_record) {
+            return 0.5 * Vector3::new(hit_record.normal.x + 1.0,
+                                      hit_record.normal.y + 1.0,
+                                      hit_record.normal.z + 1.0);
         }
-
         let unit_direction: Vector3<f64> = self.direction.normalize();
         let point: f64 = 0.5 * (unit_direction.y + 1.0);
 
@@ -30,19 +30,5 @@ impl Ray {
 
     pub fn point_at_perimeter(&self, point: f64) -> Vector3<f64> {
         self.origin + point * self.direction
-    }
-
-    pub fn hit_sphere(&self, center: &Vector3<f64>, radius: f64) -> f64 {
-        let sphere_center: Vector3<f64> = self.origin - center;
-        let a: f64 = self.direction.dot(&self.direction);
-        let b: f64 = 2.0 * sphere_center.dot(&self.direction);
-        let c: f64 = sphere_center.dot(&sphere_center) - (radius * radius);
-        let discriminant: f64 = b * b - 4.0 * a * c;
-
-        if discriminant < 0.0 {
-            return -1.0;
-        } else {
-            return (-b - discriminant.sqrt()) / (2.0 * a);
-        }
     }
 }
