ce94c7898f9a02a8d7a55551911c6cdf537531d7
mandeep <mandeep@users.noreply.github.com>
2019-03-29 13:29:31 -0400

    Add doc comments to hitable functions

--- src/hitable.rs
+++ src/hitable.rs
@@ -4,6 +4,8 @@ use materials::Material;
 use ray::Ray;
 
 
+/// HitRecord contains the elements necessary to render geometry
+/// once a ray has hit that geometry.
 pub struct HitRecord {
     pub parameter: f64,
     pub point: Vector3<f64>,
@@ -13,6 +15,7 @@ pub struct HitRecord {
 
 
 impl HitRecord {
+    /// Create a new HitRecord for a given ray-geometry intersection.
     pub fn new(parameter: f64,
                point: Vector3<f64>,
                normal: Vector3<f64>,
@@ -22,6 +25,12 @@ impl HitRecord {
 }
 
 
+/// The Hitable trait is a trait that all hitable objects will implement.
+/// This way we can easily add different types of geometry to the renderer/
 pub trait Hitable: Send + Sync {
+    /// Determine if the ray records a hit.
+    ///
+    /// We use position_min and position_max to omit points on the ray
+    /// near zero. This helps in reducing noise.
     fn hit(&self, ray: &Ray, position_min: f64, position_max: f64) -> Option<HitRecord>;
 }
