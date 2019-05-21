4f800e44c17a196bbe81196ad20b9274608afc6d
mandeep <mandeep@users.noreply.github.com>
2019-05-20 22:23:56 -0700

    Recreate scene from book

--- src/main.rs
+++ src/main.rs
@@ -28,47 +28,81 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let samples: u32 = args[1].parse().unwrap();
 
-    let origin = Vector3::new(0.0, 3.0, 6.0);
-    let lookat = Vector3::new(0.0, 0.0, -1.5);
+    let origin = Vector3::new(13.0, 2.0, 3.0);
+    let lookat = Vector3::new(0.0, 0.0, 0.0);
     let camera = Camera::new(origin,
                              lookat,
                              Vector3::new(0.0, 1.0, 0.0),
                              20.0,
                              (width / height) as f64,
-                             0.01,
+                             0.1,
                              10.0);
 
     let mut world = World::new();
 
     world.add(Sphere::new(
-        Vector3::new(0.6, 0.0, -1.0),
-        0.5,
-        Diffuse::new(Vector3::new(0.75, 0.25, 0.25))
-    ));
-
-    world.add(Sphere::new(
-        Vector3::new(-0.6, 0.0, -1.0),
-        0.5,
-        Reflective::new(Vector3::new(0.5, 0.5, 0.5), 0.1),
-    ));
-
-    world.add(Sphere::new(
-        Vector3::new(0.0, 0.1, -2.0),
-        0.5,
-        Refractive::new(Vector3::new(0.9, 0.9, 0.9), 1.5, 0.0))
-    );
-
-    world.add(Sphere::new(
-        Vector3::new(0.0, 0.1, -2.0),
-        -0.49,
-        Refractive::new(Vector3::new(0.9, 0.9, 0.9), 1.5, 0.0))
-    );
-
-    world.add(Sphere::new(
-        Vector3::new(0.0, -100.5, -1.0),
-        100.0,
+        Vector3::new(0.0, -1000.0, 0.0),
+        1000.0,
         Diffuse::new(Vector3::new(0.5, 0.5, 0.5)))
-    );
+        );
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let material = rand::random::<f64>();
+            let center: Vector3<f64> = Vector3::new(a as f64 + 0.9 * rand::random::<f64>(),
+                                                    0.2,
+                                                    b as f64 + 0.9 * rand::random::<f64>());
+
+            if (center - Vector3::new(4.0, 0.2, 0.0)).norm() > 0.9 {
+                if material < 0.8 {
+                    world.add(Sphere::new(center, 0.2, Diffuse::new(
+                                Vector3::new(
+                                    rand::random::<f64>() * rand::random::<f64>(),
+                                    rand::random::<f64>() * rand::random::<f64>(),
+                                    rand::random::<f64>() * rand::random::<f64>()))));
+                }
+                else if material < 0.95 {
+                    world.add(Sphere::new(center, 0.2, Reflective::new(
+                                Vector3::new(
+                                    0.5 * (1.0 * rand::random::<f64>()),
+                                    0.5 * (1.0 * rand::random::<f64>()),
+                                    0.5 * (1.0 * rand::random::<f64>())),
+                                    0.5 * rand::random::<f64>())));
+                }
+                else {
+                    world.add(Sphere::new(center, 0.2, Refractive::new(
+                                Vector3::new(0.9, 0.9, 0.9), 1.5, 0.0)));
+
+                    world.add(Sphere::new(center, -0.19, Refractive::new(
+                                Vector3::new(0.9, 0.9, 0.9), 1.5, 0.0)));
+                }
+            }
+        }
+    }
+
+    world.add(Sphere::new(
+        Vector3::new(-2.0, 1.0, 0.0),
+        1.0,
+        Diffuse::new(Vector3::new(0.75, 0.25, 0.25)))
+        );
+
+    world.add(Sphere::new(
+        Vector3::new(0.0, 1.0, 0.0),
+        1.0,
+        Refractive::new(Vector3::new(1.0, 1.0, 1.0), 1.5, 0.0))
+        );
+
+    world.add(Sphere::new(
+        Vector3::new(0.0, 1.0, 0.0),
+        -0.99,
+        Refractive::new(Vector3::new(1.0, 1.0, 1.0), 1.5, 0.0))
+        );
+
+    world.add(Sphere::new(
+        Vector3::new(2.0, 1.0, 0.0),
+        1.0,
+        Reflective::new(Vector3::new(0.5, 0.5, 0.5), 0.1))
+        );
 
     let mut pixels = vec![image::Rgb([0, 0, 0]); (width * height) as usize];
     pixels.par_iter_mut().enumerate().for_each(|(i, pixel)| {
