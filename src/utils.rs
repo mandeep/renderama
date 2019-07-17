1e69f71e40311f89d31f22393ffe269b714e27aa
mandeep <mandeep@users.noreply.github.com>
2019-07-16 18:36:43 -0700

    Refactor format_time to use format identifiers

--- src/utils.rs
+++ src/utils.rs
@@ -3,11 +3,8 @@ use std::time::Duration;
 pub fn format_time(instant: Duration) -> String {
     let seconds = instant.as_secs();
     let hours = seconds / 3600;
-    let minutes = (seconds - (hours * 3600)) / 60;
+    let minutes = (seconds % 3600) / 60;
+    let seconds = seconds % 60;
 
-    let hours_fmt = if hours < 10 { format!("0{}", hours.to_string()) } else { hours.to_string() };
-    let minutes_fmt = if minutes < 10 { format!("0{}", minutes.to_string()) } else { minutes.to_string() };
-    let seconds_fmt = if seconds < 10 { format!("0{}", seconds.to_string()) } else { seconds.to_string() };
-
-    format!("{}:{}:{}", hours_fmt, minutes_fmt, seconds_fmt)
+    format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
 }
