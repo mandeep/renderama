800f133ccb970e58819ad62410098cb94893ce74
mandeep <mandeep@users.noreply.github.com>
2019-08-04 13:48:42 -0700

    Remove unnecessary code blocks in hit function

--- src/volume.rs
+++ src/volume.rs
@@ -25,25 +25,24 @@ impl Volume {
 
 impl Hitable for Volume {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
-        if let Some(mut hit1) = self.boundary.hit(ray, f32::MIN, f32::MAX) {
-            if let Some(mut hit2) = self.boundary.hit(ray, hit1.parameter + 0.0001, f32::MAX) {
+        if let Some(mut hit1) = self.boundary.hit(&ray, f32::MIN, f32::MAX) {
+            if let Some(mut hit2) = self.boundary.hit(&ray, hit1.parameter + 0.0001, f32::MAX) {
                 if hit1.parameter < t_min { hit1.parameter = t_min };
                 if hit2.parameter > t_max { hit2.parameter = t_max };
-                if hit1.parameter >= hit2.parameter { return None };
-                if hit1.parameter < 0.0 { hit1.parameter = 0.0 };
+                if hit1.parameter < hit2.parameter {
+                    let distance_inside_boundary = (hit2.parameter - hit1.parameter) * ray.direction.norm();
+                    let hit_distance = -(1.0 / self.density) * rand::random::<f32>().ln();
 
-                let distance_inside_boundary = (hit2.parameter - hit1.parameter) * ray.direction.norm();
-                let hit_distance = -(1.0 / self.density) * rand::random::<f32>().ln();
-
-                if hit_distance < distance_inside_boundary {
-                    let root = hit1.parameter + hit_distance / ray.direction.norm();
-                    let point = ray.point_at_parameter(root);
-                    let normal = Vector3::new(1.0, 0.0, 0.0);
-                    return Some(HitRecord::new(root, 0.0, 0.0, point, normal, self.material.clone()));
+                    if hit_distance < distance_inside_boundary {
+                        let t = hit1.parameter + hit_distance / ray.direction.norm();
+                        let point = ray.point_at_parameter(t);
+                        let normal = Vector3::new(1.0, 0.0, 0.0);
+                        return Some(HitRecord::new(t, 0.0, 0.0, point, normal, self.material.clone()));
+                    }
                 }
             }
         }
-        return None;
+        None
     }
 
     fn bounding_box(&self, t0: f32, t1: f32) -> Option<AABB> {
