4dcdd3f7f8014b722708f9fbb76a28e3b3a6c632
mandeep <mandeep@users.noreply.github.com>
2019-08-04 13:51:39 -0700

    Add scene that shows all additions from the 2nd minibook

--- src/scene.rs
+++ src/scene.rs
@@ -12,6 +12,7 @@ use rectangle::Rectangle;
 use sphere::Sphere;
 use texture::{ConstantTexture, ImageTexture};
 use transformations::{Rotate, Translate};
+use volume::Volume;
 use world::World;
 
 pub fn three_spheres_scene(width: u32, height: u32) -> (Camera, BVH) {
@@ -404,4 +405,120 @@ pub fn cornell_box_scene(width: u32, height: u32) -> (Camera, BVH) {
     let bvh = BVH::new(&mut world.objects, 0.0, 1.0);
 
     (camera, bvh)
+}
+
+
+pub fn spheres_in_box_scene(width: u32, height: u32) -> (Camera, BVH) {
+    let origin = Vector3::new(478.0, 278.0, -600.0);
+    let lookat = Vector3::new(278.0, 278.0, 0.0);
+    let view = Vector3::new(0.0, 1.0, 0.0);
+    let fov = 40.0;
+    let aspect_ratio = (width / height) as f32;
+    let aperture = 0.0;
+    let focus_distance = 10.0;
+    let time0 = 0.0;
+    let time1 = 1.0;
+    let atmosphere = false;
+
+    let camera = Camera::new(origin,
+                             &lookat,
+                             &view,
+                             fov,
+                             aspect_ratio,
+                             aperture,
+                             focus_distance,
+                             time0,
+                             time1,
+                             atmosphere);
+
+    let mut world = World::new();
+
+    let white = Diffuse::new(ConstantTexture::new(0.73, 0.73, 0.73));
+    let red = Diffuse::new(ConstantTexture::new(0.70, 0.30, 0.10));
+    let light = Light::new(ConstantTexture::new(7.0, 7.0, 7.0));
+    let ground = Diffuse::new(ConstantTexture::new(0.48, 0.83, 0.53));
+
+    let number_of_boxes = 20;
+
+    for i in 0..number_of_boxes {
+        for j in 0..number_of_boxes {
+            let w = 100.0;
+            let p0 = Vector3::new(-1000.0 + i as f32 * w, 0.0, -1000.0 + j as f32 * w);
+            let p1 = p0 + Vector3::new(w, 100.0 * (rand::random::<f32>() + 0.01), w);
+            world.add(Rectangle::new(p0, p1, Arc::new(ground.clone())));
+        }
+    }
+
+    world.add(Plane::new(Axis::XZ, 123.0, 423.0, 147.0, 412.0, 554.0, light));
+
+    world.add(Sphere::new(Vector3::new(400.0, 400.0, 200.0), Vector3::new(430.0, 400.0, 200.0), 50.0, red, 0.0, 1.0));
+
+    world.add(Sphere::new(Vector3::new(260.0, 150.0, 45.0), Vector3::new(260.0, 150.0, 45.0), 50.0, Refractive::new(1.5), 0.0, 1.0));
+
+    world.add(Sphere::new(Vector3::new(0.0, 150.0, 145.0),
+                          Vector3::new(0.0, 150.0, 145.0),
+                          50.0,
+                          Reflective::new(Vector3::new(0.8, 0.8, 0.9), 1.0),
+                          0.0,
+                          1.0));
+
+    let boundary = Sphere::new(
+        Vector3::new(360.0, 150.0, 145.0),
+        Vector3::new(360.0, 150.0, 145.0),
+        70.0,
+        Refractive::new(1.5),
+        0.0,
+        1.0);
+
+    world.add(boundary.clone());
+
+    world.add(Volume::new(0.2, boundary.clone(), ConstantTexture::new(0.2, 0.4, 0.9)));
+
+    let fog = Sphere::new(
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        5000.0,
+        Refractive::new(1.5),
+        0.0,
+        1.0);
+
+    world.add(Volume::new(0.0001, fog, ConstantTexture::new(1.0, 1.0, 1.0)));
+
+    world.add(Sphere::new(Vector3::new(400.0, 200.0, 400.0),
+                          Vector3::new(400.0, 200.0, 400.0),
+                          100.0,
+                          Diffuse::new(ImageTexture::new("world_topo_nasa.jpg")),
+                          0.0,
+                          1.0));
+
+    world.add(Sphere::new(Vector3::new(220.0, 280.0, 300.0),
+                          Vector3::new(220.0, 280.0, 300.0),
+                          80.0,
+                          Diffuse::new(ConstantTexture::new(0.8, 0.8, 0.8)),
+                          0.0,
+                          1.0));
+
+    let number_of_spheres = 1000;
+    for _ in 0..number_of_spheres {
+        let sphere = Sphere::new(
+                Vector3::new(165.0 * rand::random::<f32>(),
+                             165.0 * rand::random::<f32>(),
+                             165.0 * rand::random::<f32>()),
+
+                Vector3::new(165.0 * rand::random::<f32>(),
+                             165.0 * rand::random::<f32>(),
+                             165.0 * rand::random::<f32>()),
+                10.0,
+                white.clone(),
+                0.0,
+                1.0);
+
+        world.add(Translate::new(Vector3::new(-100.0, 270.0, 395.0), Rotate::new(15.0, sphere)));
+    }
+
+
+    let bvh = BVH::new(&mut world.objects, 0.0, 1.0);
+
+    (camera, bvh)
+
 }
