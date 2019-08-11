92832eec57915f9c86d937193ea3e7ee9965d4ab
mandeep <mandeep@users.noreply.github.com>
2019-08-10 18:17:43 -0700

    Add doc comments to format_time and rename seconds variable

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 use std::time::Duration;
 
+/// Convert a Duration to a String formatted as HH:MM:SS
 pub fn format_time(instant: Duration) -> String {
-    let seconds = instant.as_secs();
-    let hours = seconds / 3600;
-    let minutes = (seconds % 3600) / 60;
-    let seconds = seconds % 60;
+    let total_seconds = instant.as_secs();
+    let hours = total_seconds / 3600;
+    let minutes = (total_seconds % 3600) / 60;
+    let seconds = total_seconds % 60;
 
     format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
 }
