81071eb1f98a83a8bb064c6e0492ad1ceddd5840
mandeep <mandeep@users.noreply.github.com>
2019-08-27 21:31:06 -0400

    Increase render dimensions

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ use denoise::denoise;
 fn main() {
     let rendering_time = Instant::now();
 
-    let (width, height): (u32, u32) = (1000, 1000);
+    let (width, height): (u32, u32) = (2048, 2048);
     let args: Vec<String> = env::args().collect();
     let samples: u32 = args[1].parse().unwrap();
 
