b18be251ea09320889829d527b3b1377da42e2b1
mandeep <mandeep@users.noreply.github.com>
2019-09-30 14:15:18 -0400

    Add adaptive logarithmic mapping for tone mapping

--- src/utils.rs
+++ src/utils.rs
@@ -22,6 +22,33 @@ pub fn clamp(n: f32) -> f32 {
     n.min(255.0).max(0.0)
 }
 
+/// Tone map the given luminance globally with the adaptive logarithmic mapping algorithm
+///
+/// luminance is the pixel to map, max_luminance
+/// is the smallest luminance that will be mapped to pure
+/// white, display_luminance is the maximum luminance capability of the display,
+/// and bias is term that adjusts compression of high values and visibility details
+/// in dark ares.
+///
+/// Generally, max_luminance is set to the maximum luminance in the scene,
+/// display_luminance is between 100-230 cd/m^2, and bias by default can be 0.73.
+///
+/// The tone mapping derivation can be found in Drago et al.
+/// Adaptive Logarithmic Mapping for Displaying High Constrast Scenes.
+pub fn adaptive_log_map(luminance: f32, max_luminance: f32, display_luminance: f32, bias: f32) -> f32 {
+    let bias_term = bias.ln() / 0.5f32.ln();
+
+    let first_term_numerator = display_luminance * 0.01;
+    let first_term_denominator = (max_luminance + 1.0).log10();
+    let first_term = first_term_numerator / first_term_denominator;
+
+    let second_term_numerator = (luminance + 1.0).ln();
+    let second_term_denominator = (2.0 + (luminance / max_luminance).powf(bias_term) * 8.0).ln();
+    let second_term = second_term_numerator / second_term_denominator;
+
+    first_term * second_term
+}
+
 /// Tone map the given luminance globally with the Stockham equation
 ///
 /// luminance is the pixel to map and max_luminance
