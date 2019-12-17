1d5a383e17140334282bf948d40d49f5c16ea7e9
mandeep <mandeep@users.noreply.github.com>
2019-12-16 19:09:59 -0500

    Render image as HDR in RGBE file format

--- src/denoise.rs
+++ src/denoise.rs
@@ -1,16 +1,13 @@
 #[cfg(feature = "denoise")]
 /// Denoise the input buffer and return a denoised buffer
 /// Reference: https://github.com/Twinklebear/oidn-rs/blob/master/examples/simple/src/main.rs
-pub fn denoise(input: &Vec<u8>, width: usize, height: usize) -> Vec<u8> {
-    // OIDN works on float images only, so convert this to a floating point image
-    let coerced_input: Vec<f32> = input.iter().map(|&n| n as f32 / 255.0).collect();
-
-    let mut filter_output = vec![0.0f32; coerced_input.len()];
+pub fn denoise(input: &Vec<f32>, width: usize, height: usize) -> Vec<u8> {
+    let mut filter_output = vec![0.0f32; input.len()];
 
     let mut device = oidn::Device::new();
     let mut filter = oidn::RayTracing::new(&mut device);
     filter.set_srgb(true).set_img_dims(width, height);
-    filter.execute(&coerced_input[..], &mut filter_output[..]);
+    filter.execute(&input[..], &mut filter_output[..]);
 
     if let Err(e) = device.get_error() {
         println!("Error denosing image: {}", e.1);
