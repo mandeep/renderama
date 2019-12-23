53adb606a1a5a3d1d9c9994337bc806e47cfe837
mandeep <mandeep@users.noreply.github.com>
2019-12-23 16:23:00 -0500

    Output f32 buffer instead of u8 buffer

--- src/denoise.rs
+++ src/denoise.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "denoise")]
 /// Denoise the input buffer and return a denoised buffer
 /// Reference: https://github.com/Twinklebear/oidn-rs/blob/master/examples/simple/src/main.rs
-pub fn denoise(input: &Vec<f32>, width: usize, height: usize) -> Vec<u8> {
+pub fn denoise(input: &Vec<f32>, width: usize, height: usize) -> Vec<f32> {
     let mut filter_output = vec![0.0f32; input.len()];
 
     let mut device = oidn::Device::new();
@@ -13,9 +13,5 @@ pub fn denoise(input: &Vec<f32>, width: usize, height: usize) -> Vec<u8> {
         println!("Error denosing image: {}", e.1);
     }
 
-    let output_buffer = filter_output.iter()
-                                     .map(|&color| (255.0 * color) as u8)
-                                     .collect();
-
-    output_buffer
+    filter_output
 }
