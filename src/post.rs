842fcee6c197bce7773c50d27778546e0b315c35
mandeep <mandeep@users.noreply.github.com>
2019-12-23 20:47:52 -0500

    Add blur image function that blurs image n times

--- src/post.rs
+++ src/post.rs
@@ -2,6 +2,18 @@ use image::ImageBuffer;
 
 use utils;
 
+
+pub fn blur_image(image: ImageBuffer<image::Rgb<f32>, Vec<f32>>, n: usize) -> ImageBuffer<image::Rgb<f32>, Vec<f32>> {
+    if n == 0 {
+        image
+    } else {
+        let buffer = image::imageops::blur(&image, 8.0);
+        blur_image(buffer, n - 1)
+    }
+
+}
+
+
 pub fn bloom_filter(buffer: &Vec<f32>, width: usize, height: usize) -> Vec<f32> {
     let min_luminance = utils::f32_min(&buffer);
     let max_luminance = utils::f32_max(&buffer);
@@ -19,7 +31,7 @@ pub fn bloom_filter(buffer: &Vec<f32>, width: usize, height: usize) -> Vec<f32> 
     let high_pass_buffer: ImageBuffer<image::Rgb<f32>, Vec<f32>> =
         ImageBuffer::from_vec(width as u32, height as u32, high_pass).unwrap();
 
-    let blurred_buffer = image::imageops::blur(&high_pass_buffer, 8.0);
+    let blurred_buffer = blur_image(high_pass_buffer, 5);
 
     let mut bloom = vec![0.0f32; buffer.len()];
 
