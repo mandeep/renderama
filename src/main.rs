7b7f09e2654d6ad21f3b3f400a7fcf0cf0d258b3
mandeep <10521687+mandeep@users.noreply.github.com>
2026-04-24 02:10:05 -0400

    Change default resolution and default samples

--- src/main.rs
+++ src/main.rs
@@ -55,12 +55,12 @@ use denoise::denoise;
 fn main() {
     let rendering_time = Instant::now();
 
-    let (width, height): (usize, usize) = (2048, 2048);
+    let (width, height): (usize, usize) = (800, 800);
     let args: Vec<String> = env::args().collect();
-    let samples: u32 = args[1].parse().unwrap();
+    let samples: u32 = args.get(1).and_then(|s| s.parse().ok()).unwrap_or(128);
     let bounces: u32 = 10;
 
-    let (name, camera, world, light_source) = scene::cornell_box_scene(width, height);
+    let (name, camera, world, light_source) = scene::cornell_box_bunny_scene(width, height);
 
     let render_start_time: DateTime<Local> = Local::now();
     println!("[{}] Rendering '{}' scene with {} samples at {} x {} dimensions...",
