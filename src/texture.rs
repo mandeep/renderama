bf5be128dbd37eac42593e5ba806ad01ad18c22f
mandeep <10521687+mandeep@users.noreply.github.com>
2026-04-29 18:26:15 -0400

    Add scale parameter to imagetexture

--- src/texture.rs
+++ src/texture.rs
@@ -56,19 +56,23 @@ impl SolidColor {
 /// ImageTexture is a struct for textures loaded from file
 pub struct ImageTexture {
     im: image::RgbImage,
+    scale: f32,
 }
 
 /// Create a new texture from the given data and image dimensions
 impl ImageTexture {
-    pub fn new(filename: &str) -> ImageTexture {
-        ImageTexture { im: image::open(filename).unwrap().flipv().to_rgb8() }
+    pub fn new(filename: &str, scale: f32) -> ImageTexture {
+        ImageTexture { im: image::open(filename).unwrap().flipv().to_rgb8(), scale: scale }
     }
 
     /// Determine which pixel to retrieve from the image by
     /// converting pixel coordinates to UV coordinates
     pub fn value(&self, u: f32, v: f32, _p: &Vec3) -> Vec3 {
-        let i = 0.0f32.max((u * self.im.width() as f32).min(self.im.width() as f32 - 1.0));
-        let j = 0.0f32.max((v * self.im.height() as f32).min(self.im.height() as f32 - 1.0));
+        let u_scaled = (u * self.scale) % 1.0;
+        let v_scaled = (v * self.scale) % 1.0;
+
+        let i = 0.0f32.max((u_scaled * self.im.width() as f32).min(self.im.width() as f32 - 1.0));
+        let j = 0.0f32.max((v_scaled * self.im.height() as f32).min(self.im.height() as f32 - 1.0));
 
         let image::Rgb([r, g, b]) = *self.im.get_pixel(i as u32, j as u32);
 
