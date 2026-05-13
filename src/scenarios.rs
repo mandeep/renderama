b1b990b7451b69d24f32e4fc6c2d8edeb9483dba
mandeep <10521687+mandeep@users.noreply.github.com>
2026-05-12 20:45:53 -0400

    Add three spheres scene for testing materials

--- src/scenarios.rs
+++ src/scenarios.rs
@@ -7,6 +7,7 @@ mod cornell_box_dragon;
 mod cornell_box_objects;
 mod random_spheres;
 mod spheres_in_box;
+mod three_spheres;
 mod veach_mis;
 
 pub use self::cornell_box_boxes::*;
@@ -15,6 +16,7 @@ pub use self::cornell_box_dragon::*;
 pub use self::cornell_box_objects::*;
 pub use self::random_spheres::*;
 pub use self::spheres_in_box::*;
+pub use self::three_spheres::*;
 pub use self::veach_mis::*;
 
 
@@ -26,6 +28,7 @@ pub enum Scenario {
     CornellBoxObjects,
     RandomSpheres,
     SpheresInBox,
+    ThreeSpheres,
     VeachMis,
 }
 
@@ -38,6 +41,7 @@ impl Scenario {
             "cornell_box_objects" => Some(Scenario::CornellBoxObjects),
             "random_spheres" => Some(Scenario::RandomSpheres),
             "spheres_in_box" => Some(Scenario::SpheresInBox),
+            "three_spheres" => Some(Scenario::ThreeSpheres),
             "veach_mis" => Some(Scenario::VeachMis),
             _ => None,
         }
@@ -51,6 +55,7 @@ impl Scenario {
             Scenario::CornellBoxObjects => cornell_box_object_scene(width, height),
             Scenario::RandomSpheres => random_spheres_scene(width, height),
             Scenario::SpheresInBox => spheres_in_box_scene(width, height),
+            Scenario::ThreeSpheres => three_spheres_scene(width, height),
             Scenario::VeachMis => veach_mis_scene(width, height),
         }
     }
---
+++ src/scenarios/three_spheres.rs
@@ -0,0 +1,60 @@
+use std::f32;
+
+use glam::Vec3A;
+
+use bvh::BVH;
+use camera::Camera;
+use materials::{Diffuse, Reflective, Refractive, Material};
+use primitive::Primitive;
+use scene::Scene;
+use sphere::Sphere;
+use texture::{EnvironmentMap, SolidColor};
+use world::World;
+use mat;
+
+
+pub fn three_spheres_scene(width: Option<usize>, height: Option<usize>) -> Scene {
+    let origin = Vec3A::new(0.0, 3.0, 6.0);
+    let lookat = Vec3A::new(0.0, 0.0, -1.5);
+    let view = Vec3A::new(0.0, 1.0, 0.0);
+    let fov = 20.0;
+    let aspect_ratio = (width.unwrap_or(2048) as f32, height.unwrap_or(1024) as f32);
+    let aperture = 0.01;
+    let focus_distance = (lookat - origin).length();
+
+    let camera = Camera::new(origin,
+                             lookat,
+                             view,
+                             fov,
+                             aspect_ratio,
+                             aperture,
+                             focus_distance);
+
+    let mut world = World::new();
+    let mut materials: Vec<Material> = Vec::new();
+
+    let red_idx = mat!(materials, Diffuse::new(SolidColor::new(0.75, 0.25, 0.25).into(), 0.0));
+
+
+    world.add(Primitive::Sphere(Sphere::new(Vec3A::new(0.6, 0.0, -1.0),
+                          0.5,
+                          red_idx)));
+
+    let metal_idx = mat!(materials, Reflective::new(Vec3A::new(0.5, 0.5, 0.5), 0.01));
+
+    world.add(Primitive::Sphere(Sphere::new(Vec3A::new(-0.6, 0.0, -1.0),
+                          0.5,
+                          metal_idx)));
+
+    let metal_idx2 = mat!(materials, Reflective::new(Vec3A::new(0.5, 0.5, 0.5), 0.2));
+
+    world.add(Primitive::Sphere(Sphere::new(Vec3A::new(0.0, 0.1, -2.0),
+                          0.5,
+                          metal_idx2)));
+
+    let bvh = BVH::new(&mut world.objects, 0.0, 1.0);
+
+    let environment = EnvironmentMap::new("docs/textures/golden_gate_hills.exr").into();
+
+    Scene::new(String::from("Three Spheres"), bvh, materials, camera, vec![], Some(environment), false)
+}
