45c95c2de8407f80c9ead00b0f31d166a40a6e83
mandeep <10521687+mandeep@users.noreply.github.com>
2026-05-14 11:24:59 -0400

    Change aspect ratio and move camera closer to spheres

--- src/scenes/roughness_energy_loss.rs
+++ src/scenes/roughness_energy_loss.rs
@@ -17,8 +17,8 @@ pub fn albedo_one_sphere_scene(width: Option<usize>, height: Option<usize>) -> S
     let origin = Vec3A::new(278.0, 278.0, -500.0);
     let lookat = Vec3A::new(278.0, 278.0, 300.0);
     let view = Vec3A::new(0.0, 1.0, 0.0);
-    let fov = 40.0;
-    let aspect_ratio = (width.unwrap_or(2048) as f32, height.unwrap_or(2048) as f32);
+    let fov = 10.0;
+    let aspect_ratio = (width.unwrap_or(2048) as f32, height.unwrap_or(512) as f32);
 
     let camera = Camera::new(origin, lookat, view, fov, aspect_ratio, 0.0, 10.0);
 
