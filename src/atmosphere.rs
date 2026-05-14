12c47b0cc1ced525a021b40f928bfd7274b1ee7a
mandeep <10521687+mandeep@users.noreply.github.com>
2026-05-14 11:24:37 -0400

    Add default impl for Atmosphere

--- src/atmosphere.rs
+++ src/atmosphere.rs
@@ -13,11 +13,20 @@ impl Atmosphere {
     pub fn compute_atmosphere_color(&self, direction: &Vec3A) -> Vec3A {
         if self.lerp {
             let point: f32 = 0.5 * (direction.y + 1.0);
-            // first implementation used (0.5, 0.7, 1.0) as background color
             let lerp = (1.0 - point) * Vec3A::splat(1.0) + point * self.background;
             return lerp;
         }
 
         self.background
+    }
+}
+
+impl Default for Atmosphere {
+    fn default() -> Atmosphere {
+        Atmosphere {
+            // first implementation used (0.5, 0.7, 1.0) as background color
+            background: Vec3A::new(0.5, 0.7, 1.0),
+            lerp: true,
+        }
     }
 }
