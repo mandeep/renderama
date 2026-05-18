84fcdd15ce17c0406e79363923b0d4fe72d91f20
mandeep <10521687+mandeep@users.noreply.github.com>
2026-05-17 23:52:42 -0400

    Continue work on hyperion render by adding more spheres

--- src/scenes/hyperion.rs
+++ src/scenes/hyperion.rs
@@ -5,22 +5,25 @@ use glam::Vec3A;
 use bvh::BVH;
 use camera::Camera;
 use environment::EnvironmentMap;
-use materials::{Diffuse, Reflective, Refractive, Material};
+use materials::{Diffuse, Material, Plastic, Reflective, Refractive, Volumetric};
 use plane::{Axis, Bounds2D, Plane};
 use primitive::Primitive;
 use rectangle::Rectangle;
 use scene::Scene;
 use sphere::Sphere;
 use texture::Color;
+use transformations::TransformedMesh;
+use triangle::TriangleMesh;
+use volume::Volume;
 
 use mat;
 
 
 pub fn hyperion_scene(width: Option<usize>, height: Option<usize>) -> Scene {
-    let origin = Vec3A::new(0.0, 3.0, 5.0);
-    let lookat = Vec3A::new(0.0, 0.0, -1.5);
+    let origin = Vec3A::new(0.0, 6.0, 6.0);
+    let lookat = Vec3A::new(0.0, 0.0, -1.0);
     let view = Vec3A::new(0.0, 1.0, 0.0);
-    let fov = 39.0;
+    let fov = 29.0;
     let aspect_ratio = (width.unwrap_or(2048) as f32, height.unwrap_or(1024) as f32);
     let aperture = 0.01;
     let focus_distance = (lookat - origin).length();
@@ -36,25 +39,42 @@ pub fn hyperion_scene(width: Option<usize>, height: Option<usize>) -> Scene {
     let mut objects: Vec<Primitive> = Vec::new();
     let mut materials: Vec<Material> = Vec::new();
 
-    let metal_idx3 = mat!(materials, Refractive::new(1.5, Vec3A::ONE.into()));
-    objects.push(Sphere::new(Vec3A::new(0.6, 0.70, -1.0), 0.5, metal_idx3).into());
-
-    let metal_idx = mat!(materials, Reflective::new(Vec3A::new(0.93, 0.93, 0.93), 0.0));
-    objects.push(Sphere::new(Vec3A::new(-0.6, 0.70, -1.0), 0.5, metal_idx).into());
-
-    let metal_idx2 = mat!(materials, Reflective::new(Vec3A::new(0.6, 0.6, 0.6), 0.2));
-    objects.push(Sphere::new(Vec3A::new(0.0, 0.70, -2.0), 0.5, metal_idx2).into());
-
+    let orange_color = Color::new(1.0, 0.32, 0.0);
     let floor_idx = mat!(materials, Diffuse::new(Color::new(0.5, 0.5, 0.5).into(), 0.0));
-    objects.push(Plane::new(Axis::XZ, Bounds2D::new(-50.0..50.0, -50.0..50.0), 0.0, floor_idx).into());
-    
+    let glass_idx = mat!(materials, Refractive::new(1.5, Vec3A::ONE.into()));
+    let metal_idx = mat!(materials, Reflective::new(Vec3A::new(0.93, 0.93, 0.93), 0.12));
     let box_idx = mat!(materials, Diffuse::new(Color::new(0.75, 0.75, 0.75).into(), 0.0));
-    let layout_box = Rectangle::new(Vec3A::new(-3.0, 0.0, -3.5), Vec3A::new(3.0, 0.2, 1.5), box_idx);
+    // let vol_idx = mat!(materials, Volumetric::new(orange_color.into()));
+    let orange_idx = mat!(materials, Plastic::new(orange_color.into(), 0.25, 1.45));
+    let marble_idx = mat!(materials, Refractive::new(1.5, Vec3A::new(0.25, 0.48, 0.29)));
+
+    let floor_plane = Plane::new(Axis::XZ, Bounds2D::new(-50.0..50.0, -50.0..50.0), 0.0, floor_idx);
+    let layout_box = Rectangle::new(Vec3A::new(-3.5, 0.0, -4.0), Vec3A::new(3.5, 0.2, 1.0), box_idx);
+    let glass_sphere = Sphere::new(Vec3A::new(2.0, 0.60, -1.0), 0.4, glass_idx);
+    // let orange_sphere = Sphere::new(Vec3A::new(1.0, 0.56, -2.0), 0.4, glass_idx);
+    let orange_sphere = Sphere::new(Vec3A::new(1.0, 0.55, -2.0), 0.35, orange_idx);
+    let metal_sphere = Sphere::new(Vec3A::new(-2.0, 0.65, -1.0), 0.45, metal_idx);
+    // let cloned_sphere = orange_sphere.clone();
+    // let sphere_volume = Volume::new(3.0, cloned_sphere.into(), vol_idx);
+    let large_marble = Sphere::new(Vec3A::new(0.35, 0.35, 0.0), 0.15, marble_idx);
+    let small_marble = Sphere::new(Vec3A::new(-0.35, 0.30, 0.0), 0.10, marble_idx);
+
+    let ring_mesh = TriangleMesh::from("docs/models/ring.obj", metal_idx).into();
+    let ring_transformed = TransformedMesh::new(Vec3A::new(-0.5, 0.20, -1.25), Vec3A::new(0.0, 0.0, 0.0), 0.15, ring_mesh);
+
+    objects.push(floor_plane.into());
     objects.push(layout_box.into());
+    objects.push(glass_sphere.into());
+    objects.push(metal_sphere.into());
+    objects.push(orange_sphere.into());
+    // objects.push(sphere_volume.into());
+    objects.push(large_marble.into());
+    objects.push(small_marble.into());
+    objects.push(ring_transformed.into());
 
     let bvh = BVH::new(&mut objects);
 
-    let environment = EnvironmentMap::new("docs/textures/studio_small_08.exr").into();
+    let environment = EnvironmentMap::new("docs/textures/white_studio_03.exr").into();
 
     Scene::new(String::from("Hyperion"), bvh, materials, camera, vec![], Some(environment), None)
 }
