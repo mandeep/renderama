3d3d37080565458d5062f0e1f8363cd74a5835f8
mandeep <10521687+mandeep@users.noreply.github.com>
2026-06-30 19:29:12 -0400

    Add a missing light error and add messages
    Added a MissingLight member to the SceneBuildError
    enum so that the scene will not be built without the
    required lighting.

--- src/scene.rs
+++ src/scene.rs
@@ -32,8 +32,9 @@ pub struct SceneBuilder {
 
 #[derive(Debug)]
 pub enum SceneBuildError {
-    MissingCamera,
-    MissingBVH,
+    MissingCamera(String),
+    MissingBVH(String),
+    MissingLights(String),
 }
 
 impl SceneBuilder {
@@ -82,8 +83,18 @@ impl SceneBuilder {
     }
 
     pub fn build(self) -> Result<Scene, SceneBuildError> {
-        let camera = self.camera.ok_or(SceneBuildError::MissingCamera)?;
-        let accelerator = self.accelerator.ok_or(SceneBuildError::MissingBVH)?;
+        let camera = self.camera.ok_or(
+            SceneBuildError::MissingCamera("Scene requires a camera.".to_string())
+        )?;
+        let accelerator = self.accelerator.ok_or(
+            SceneBuildError::MissingBVH("Scene is missing objects to render.".to_string())
+        )?;
+
+        if self.environment.is_none() && self.lights.is_empty() && self.atmosphere.is_none() {
+            return Err(
+                SceneBuildError::MissingLights("Scene requires an atmosphere, environment map, or at least one light.".to_string())
+            );
+        }
 
         let scene = Scene {
             name: self.name,
@@ -97,5 +108,123 @@ impl SceneBuilder {
         };
 
         Ok(scene)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use glam::Vec3A;
+
+    use super::*;
+    use crate::camera::CameraOptions;
+    use crate::extensions::PushInto;
+    use crate::materials::Diffuse;
+    use crate::sphere::Sphere;
+    use crate::texture::Color;
+    use crate::{mat, tex};
+
+    #[test]
+    fn test_scene_missing_camera() {
+        let mut objects = Vec::new();
+        let mut materials: Vec<Material> = Vec::new();
+        let mut textures: Vec<Texture> = Vec::new();
+
+        let floor_id = tex!(textures, Color::new(0.5, 0.5, 0.52));
+        let floor_idx = mat!(materials, Diffuse::new(floor_id, 0.0));
+        objects.push_into(Sphere::new(Vec3A::new(0.0, -100.5, -1.0), 100.0, floor_idx));
+
+        let bvh = BVH::new(&mut objects);
+
+        let environment = EnvironmentMap::new("extras/textures/dusk_1_puresky.exr", 1.0);
+
+        let builder = SceneBuilder::new("Three Spheres")
+            .with_accelerator(bvh)
+            .with_materials(materials, textures)
+            .with_environment(environment);
+
+        let result = builder.build();
+
+        assert!(matches!(result, Err(SceneBuildError::MissingCamera(_))));
+    }
+
+    #[test]
+    fn test_scene_missing_accelerator() {
+        let origin = Vec3A::new(478.0, 278.0, -600.0);
+        let lookat = Vec3A::new(278.0, 278.0, 0.0);
+        let fov = 40.0;
+        let focus_distance = 10.0;
+        let world_scale = 1.0;
+        let fps = 24.0;
+        let frame_duration = 1.0 / fps;
+        let shutter_speed = 1.0;
+
+        let camera_options = CameraOptions::new()
+            .with_origin(origin)
+            .with_lookat(lookat)
+            .with_fov(fov)
+            .with_focus_distance(focus_distance)
+            .with_world_scale(world_scale)
+            .with_frame_duration(frame_duration)
+            .with_shutter_speed(shutter_speed)
+            .with_resolution(512, 512);
+        let camera = Camera::new(&camera_options);
+
+        let materials: Vec<Material> = Vec::new();
+        let textures: Vec<Texture> = Vec::new();
+
+        let environment = EnvironmentMap::new("extras/textures/dusk_1_puresky.exr", 1.0);
+
+        let builder = SceneBuilder::new("Three Spheres")
+            .with_camera(camera)
+            .with_materials(materials, textures)
+            .with_environment(environment);
+
+        let result = builder.build();
+
+        assert!(matches!(result, Err(SceneBuildError::MissingBVH(_))));
+    }
+
+    #[test]
+    fn test_scene_missing_lights() {
+        let origin = Vec3A::new(478.0, 278.0, -600.0);
+        let lookat = Vec3A::new(278.0, 278.0, 0.0);
+        let fov = 40.0;
+        let focus_distance = 10.0;
+        let world_scale = 1.0;
+        let fps = 24.0;
+        let frame_duration = 1.0 / fps;
+        let shutter_speed = 1.0;
+
+        let camera_options = CameraOptions::new()
+            .with_origin(origin)
+            .with_lookat(lookat)
+            .with_fov(fov)
+            .with_focus_distance(focus_distance)
+            .with_world_scale(world_scale)
+            .with_frame_duration(frame_duration)
+            .with_shutter_speed(shutter_speed)
+            .with_resolution(512, 512);
+        let camera = Camera::new(&camera_options);
+
+        let mut objects = Vec::new();
+        let mut materials: Vec<Material> = Vec::new();
+        let mut textures: Vec<Texture> = Vec::new();
+
+        let floor_id = tex!(textures, Color::new(0.5, 0.5, 0.52));
+        let floor_idx = mat!(materials, Diffuse::new(floor_id, 0.0));
+        objects.push_into(Sphere::new(Vec3A::new(0.0, -100.5, -1.0), 100.0, floor_idx));
+
+        let bvh = BVH::new(&mut objects);
+
+        let builder = SceneBuilder::new("Three Spheres")
+            .with_camera(camera)
+            .with_accelerator(bvh)
+            .with_materials(materials, textures);
+
+        let result = builder.build();
+
+        assert!(matches!(result, Err(SceneBuildError::MissingLights(_))));
+
     }
 }
